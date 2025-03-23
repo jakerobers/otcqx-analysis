fb0e07aeaa292022854524bf8f2e741488cbb2fe
Jake Robers <jake@jakerobers.com>
2025-03-23 17:22:18 -0500

    feat: Update command for inferring financial report URLs in main.py

--- src/main.py
+++ src/main.py
@@ -2,8 +2,7 @@ import argparse
 import asyncio
 from commands.get_url import get_url
 from commands.dox import dox
-from commands.infer_financial_page import infer_financial_page
-from commands.infer_financial_report_url import infer_financial_report_url
+from commands.infer_financial_report_urls import infer_financial_report_urls
 
 
 async def main():
@@ -16,14 +15,10 @@ async def main():
     dox_parser.add_argument('-i', '--input', required=True, help='Input file path')
     dox_parser.add_argument('-o', '--output', required=True, help='Output file path')
 
-    # Infer financial page command
-    infer_fin_page_parser = subparsers.add_parser('infer-fin-page', help='Infer the financial reporting page of the company')
-    infer_fin_page_parser.add_argument('-i', '--input', required=True, help='Input file path')
-    infer_fin_page_parser.add_argument('-l', '--limit', type=int, help='Limit the number of companies to process')
-
-    # Infer financial report URL command
-    infer_fin_report_url_parser = subparsers.add_parser('infer-fin-report-url', help='Infer the financial report URL of the company')
-    infer_fin_report_url_parser.add_argument('-c', '--company', required=True, help='Company name')
+    # Infer financial report URLs command
+    infer_fin_report_urls_parser = subparsers.add_parser('infer-fin-report-urls', help='Infer the financial reporting page of the company')
+    infer_fin_report_urls_parser.add_argument('-i', '--input', required=True, help='Input file path')
+    infer_fin_report_urls_parser.add_argument('-l', '--limit', type=int, help='Limit the number of companies to process')
 
     # Fetch domain for a company
     get_url_parser = subparsers.add_parser('get-url', help='Fetch the URL for a company\'s financial documents')
@@ -34,10 +29,8 @@ async def main():
 
     if args.command == 'dox':
         await dox(args.input, args.output)
-    elif args.command == 'infer-fin-page':
-        await infer_financial_page(args.input, args.limit)
-    elif args.command == 'infer-fin-report-url':
-        await infer_financial_report_url(args.company)
+    elif args.command == 'infer-fin-report-urls':
+        await infer_financial_report_urls(args.input, args.limit)
     elif args.command == 'get-url':
         await get_url(args.input, args.limit)
 
